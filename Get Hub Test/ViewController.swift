c2e4bfe3e88df45e7776a095384ecb66c84ca609
Ryan Cummins <rcummins@ryans-macbook-air-62.local>
2018-04-06 10:26:37 -0500

    Initial Commit

---
+++ Get Hub Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Get Hub Test
+//
+//  Created by Ryan Cummins on 4/6/18.
+//  Copyright © 2018 Ryan Cummins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
